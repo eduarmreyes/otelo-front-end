c2ac0c09ee7ae4c3d3a855ef8ab238561b446cd0
eduarmreyes <eduar.mreyes@gmail.com>
2017-08-25 11:40:42 -0600

    Remove fixed background image of jumbotron

--- app/components/Jumbotron/Section.js
+++ app/components/Jumbotron/Section.js
@@ -5,7 +5,7 @@ import BackgroundImage from './bg-image.png';
 const Section = styled.section`
   background-image: url(${BackgroundImage});
   background-repeat: no-repeat;
-  background-attachment: fixed;
+  background-attachment: scroll;
   background-position: bottom center;
   background-size: 100% 90%;
   height: 538px;
